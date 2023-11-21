f7680488d81a843e5de912e8eebeaa08b0a8108e
clmntgbr <clement.goubier@gmail.com>
2023-11-21 10:45:26 +0100

    fixing github pages

--- app/gas_stations/page.tsx
+++ app/gas_stations/page.tsx
@@ -6,7 +6,7 @@ import useSWR from "swr";
 const fetcher = (url: RequestInfo) => fetch(url).then((res) => res.json());
 
 
-export default function GetGasStations({ params }: { params: { uuid: string } }) {
+export default function GetGasStations() {
   const { data, error, isLoading } = useSWR(
       "https://back.traefik.me/api/gas_stations",
       fetcher
